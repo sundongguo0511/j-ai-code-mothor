63dab4206187282f037a80551a5bbe8bda0eb9b7
jelly <sundongguo0511@gmail.com>
2025-08-10 16:35:34 +0800

    后端应用模块（基础功能开发）
    1.生成应用管理相关功能

---
+++ src/main/java/com/jelly/jaicodemothor/model/dto/app/AppUpdateRequest.java
@@ -0,0 +1,24 @@
+package com.jelly.jaicodemothor.model.dto.app;
+
+import lombok.Data;
+
+import java.io.Serializable;
+
+/**
+ * 更新应用
+ */
+@Data
+public class AppUpdateRequest implements Serializable {
+
+    /**
+     * id
+     */
+    private Long id;
+
+    /**
+     * 应用名称
+     */
+    private String appName;
+
+    private static final long serialVersionUID = 1L;
+}
--- src/main/java/com/jelly/jaicodemothor/service/AppService.java
+++ src/main/java/com/jelly/jaicodemothor/service/AppService.java
@@ -1,7 +1,12 @@
 package com.jelly.jaicodemothor.service;
 
+import com.jelly.jaicodemothor.model.dto.app.AppQueryRequest;
 import com.jelly.jaicodemothor.model.entity.App;
+import com.jelly.jaicodemothor.model.vo.AppVO;
+import com.mybatisflex.core.query.QueryWrapper;
 import com.mybatisflex.core.service.IService;
+
+import java.util.List;
 
 /**
  * 应用 服务层。
@@ -9,5 +14,25 @@ import com.mybatisflex.core.service.IService;
  * @author jelly
  */
 public interface AppService extends IService<App> {
+
+    /**
+     * 应用数据脱敏
+     * @param app 应用信息
+     * @return 脱敏后的应用信息
+     */
+    AppVO getAppVO(App app);
+    /**
+     * 应用列表数据脱敏
+     * @param appList 应用列表
+     * @return 脱敏后的应用列表
+     */
+    List<AppVO> getAppVOList(List<App> appList);
+
+    /**
+     * 跟据应用查询请求参数 构建 数据查询 wrapper
+     * @param appQueryRequest 应用查询请求参数
+     * @return 数据查询 wrapper
+     */
+    QueryWrapper getQueryWrapper(AppQueryRequest appQueryRequest);
 
 }
